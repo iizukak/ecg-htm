06e780180b7907d77a6c3c3bf57a92c89f08f2f6
Kentaro Iizuka <iizuka.kentaro@gmail.com>
2015-10-29 18:24:48 +0900

    using argparse in wavelet convertion

--- src/wavelet_converter.py
+++ src/wavelet_converter.py
@@ -1,29 +1,39 @@
+import sys
 import csv
 import datetime
 import os
 import pywt
 from collections import deque
 
-from nupic.data.inference_shifter import InferenceShifter
-from nupic.frameworks.opf.modelfactory import ModelFactory
-
+# setting for wavelet conversion
 WAVELET_SEGMENT_SIZE = 256
 WAVELET_UPDATE_SIZE = 1
-WAVELET_DB = "db1"
+WAVELET_DB = "db2"
 WAVELET_MODE = "sp1"
-TARGET_FILE = "healthy_person1"
+
+# setting CSV data directory
 DATA_DIR = "data/"
 
+# setting file
+import argparse
+parser = argparse.ArgumentParser(description = "Lovelive MaU Manual Generator")
+parser.add_argument('--target', required=True)
+args = parser.parse_args()
+t = args.target
+targetPath = DATA_DIR + t + ".csv"
+outputPath = DATA_DIR + t + "_converted.csv"
+
 # setting input file
-inputFile = open(DATA_DIR + TARGET_FILE + ".csv", "r")
-csvReader = csv.reader(inputFile)
-print("DEBUG: INPUT_FILE:", inputFile)
+targetFile = open(targetPath, "r")
+csvReader = csv.reader(targetFile)
+print("DEBUG: TARGET FILE PATH:", targetFile)
 
 # setting output file
-outputFile = open(DATA_DIR + TARGET_FILE + "_converted.csv", "w")
+outputFile = open(outputPath, "w")
 csvWriter = csv.writer(outputFile, lineterminator='\n')
-print("DEBUG: OUTPUT_FILE:", outputFile)
+print("DEBUG: OUTPUT FILE PATH:", outputFile)
 
+# Skip Header File
 csvReader.next()
 csvReader.next()
 csvReader.next()
@@ -36,16 +46,16 @@ for i in range(WAVELET_SEGMENT_SIZE):
 
 cA, cD = pywt.dwt(currentSegment, WAVELET_DB, mode=WAVELET_MODE)
 
-print(cA)
-print(len(cA))
+print("DEBUG: SAMPLE CONVERTED ROW: ", cA)
+print("DEBUG: SAMPLE CONVERTED ROW SIZE:" , len(cA))
 
 csvWriter.writerow(cA)
 
-iteration_size = ((sum(1 for line in open(DATA_DIR + TARGET_FILE + ".csv", "r")) \
+iteration_size = ((sum(1 for line in open(targetPath, "r")) \
                     - WAVELET_SEGMENT_SIZE) \
                     / WAVELET_UPDATE_SIZE) - 4
 
-print(iteration_size)
+print("DEBUG: LOOP_NUM:", iteration_size)
 for i in range(iteration_size):
     for j in range(WAVELET_UPDATE_SIZE):
         date, value = csvReader.next()
@@ -54,5 +64,5 @@ for i in range(iteration_size):
     cA, cD = pywt.dwt(sorted(currentSegment), WAVELET_DB, mode=WAVELET_MODE)
     csvWriter.writerow(cA)
 
-inputFile.close()
+targetFile.close()
 outputFile.close()
