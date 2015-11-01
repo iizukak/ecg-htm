b3f2b23f89a7068e2d347ba3e1ceb7b06ed28d11
Kentaro Iizuka <iizuka.kentaro@gmail.com>
2015-11-01 11:23:47 +0900

    fft and wavelet converter test

---
+++ src/fft_converter.py
@@ -0,0 +1,47 @@
+import sys
+import csv
+import datetime
+import os
+import pywt
+import numpy
+from collections import deque
+
+# setting for fft conversion
+FFT_SEGMENT_SIZE = 500
+
+# setting CSV data directory
+DATA_DIR = "data/"
+
+# setting file
+import argparse
+parser = argparse.ArgumentParser(description = "")
+parser.add_argument('--target', required=True)
+args = parser.parse_args()
+t = args.target
+targetPath = DATA_DIR + t + ".csv"
+outputPath = DATA_DIR + t + "_wavelet_converted.csv"
+#outputPath = DATA_DIR + t + "_fft_converted.csv"
+
+# setting input file
+targetFile = open(targetPath, "r")
+csvReader = csv.reader(targetFile)
+print("DEBUG: TARGET FILE PATH:", targetPath)
+
+outputFile = open(outputPath, "w")
+csvWriter = csv.writer(outputFile, lineterminator='\n')
+print("DEBUG: OUTPUT FILE PATH:", outputPath)
+
+currentSegment = deque(maxlen = FFT_SEGMENT_SIZE)
+
+# make initial segment
+for i in range(FFT_SEGMENT_SIZE):
+    date, value = csvReader.next()
+    currentSegment.append(int(value))
+print(currentSegment)
+
+for row in csvReader:
+    date, value = row[0], int(row[1])
+    currentSegment.append(value)
+    #FFTValue = numpy.abs(numpy.fft.fft(currentSegment))
+    FFTValue = pywt.dwt(currentSegment ,"db1")[0]
+    csvWriter.writerow(FFTValue)
